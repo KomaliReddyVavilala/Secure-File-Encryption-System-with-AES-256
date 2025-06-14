c1d0509b67f1152a493ad4d945ecf4c75ff28c0e
KomaliReddyVavilala <161595708+KomaliReddyVavilala@users.noreply.github.com>
2025-06-14 16:30:09 +0530

    aes_file_crypto.py

--- aes_file_crypto.py
+++ aes_file_crypto.py
@@ -4,21 +4,15 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 import os
 import logging
-
-# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class AESFileCrypto:
     def __init__(self, password: str, salt: bytes = None):
-        """
-        Initialize the crypto system with a password and optional salt.
-        If salt is not provided, a random one will be generated.
-        """
+       
         self.password = password.encode()
         self.salt = salt if salt else os.urandom(16)
         
-        # Derive key from password using PBKDF2
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
@@ -29,10 +23,7 @@ class AESFileCrypto:
         self.cipher = Fernet(key)
     
     def encrypt_file(self, input_file_path: str, output_file_path: str = None):
-        """
-        Encrypt a file using AES-256.
-        If output path is not specified, adds '.enc' extension to input file.
-        """
+    
         if not output_file_path:
             output_file_path = input_file_path + '.enc'
         
@@ -41,8 +32,7 @@ class AESFileCrypto:
                 file_data = f.read()
             
             encrypted_data = self.cipher.encrypt(file_data)
-            
-            # Save salt along with encrypted data
+
             with open(output_file_path, 'wb') as f:
                 f.write(self.salt + encrypted_data)
             
@@ -53,10 +43,7 @@ class AESFileCrypto:
             return False
     
     def decrypt_file(self, input_file_path: str, output_file_path: str = None):
-        """
-        Decrypt a file encrypted with this system.
-        If output path is not specified, removes '.enc' extension if present.
-        """
+    
         if not output_file_path:
             if input_file_path.endswith('.enc'):
                 output_file_path = input_file_path[:-4]
@@ -69,8 +56,7 @@ class AESFileCrypto:
                 salt = f.read(16)
                 # Rest is encrypted data
                 encrypted_data = f.read()
-            
-            # Reinitialize cipher with the salt from the file
+        
             kdf = PBKDF2HMAC(
                 algorithm=hashes.SHA256(),
                 length=32,
